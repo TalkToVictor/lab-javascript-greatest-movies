52d7bda2a215a62e318b720fbc141a303cbb2366
Victor Nwani <itsvictornwani@yahoo.com>
2021-01-25 18:32:51 -0500

    yeah

--- src/movies.js
+++ src/movies.js
@@ -1,6 +1,29 @@
 // Iteration 1: All directors? - Get the array of all directors.
+function getAllDirectors(movies) {
+    const directors = movies
+        .filter((movie) => movie.director)
+        .map((movie) => movie.director);
+    const uniqueDirectors = directors.filter((elem, i, arr) => arr.indexOf(elem) === i);
+
+    return uniqueDirectors;
+}
+//Same Solution but introducing Set(), Set objects are collections of values. You can iterate through the elements of a set in insertion order. A value in the Set may only occur once; it is unique in the Set's collection.
+
+/* function getAllDirectors(movies) {
+  const directors = movies
+    .filter((movie) => movie.director)
+    .map((movie) => movie.director);
+  return [...new Set(directors)];
+} */
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
+function howManyMovies(someMovies) {
+    return someMovies.filter(
+        (eachMovie) =>
+            eachMovie.director === "Steven Spielberg" &&
+            eachMovie.genre.includes("Drama")
+    ).length;
+}
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
